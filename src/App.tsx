f4f85c761dcd95dfeb6c982e53796d8fc9c11c74
Alexander Gurnet <Alexandergurnet@gmail.com>
2022-06-15 23:40:40 +0300

    added a main app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { Navigate, Routes, Route } from 'react-router-dom';
+
+import { PostPage } from './features/PostPage';
+import { PostContainer } from './features/PostContainer';
+
+import s from './app.module.scss';
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <main className={s.container}>
+      <Routes>
+        <Route path="/posts" element={<PostContainer />} />
+        <Route path="/posts/:id" element={<PostPage />} />
+        <Route path="*" element={<Navigate to="/posts" replace />} />
+      </Routes>
+    </main>
+  );
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,22 @@
 import ReactDOM from 'react-dom/client';
-import './styles/globals.scss';
+import { BrowserRouter } from 'react-router-dom';
+
+import { SearchProvider } from './contexts/SearchContext';
+import { PostProvider } from './contexts/PostContext';
+import { PaginationProvider } from './contexts/PaginationContext';
 import App from './App';
 
+import './styles/globals.scss';
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(<App />);
+root.render(
+  <SearchProvider>
+    <PaginationProvider>
+      <PostProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PostProvider>
+    </PaginationProvider>
+  </SearchProvider>
+);
